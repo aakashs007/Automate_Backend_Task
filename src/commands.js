e1e5cfe09af70d350eae293812001eb09368d187
Aakash Singh <aakash.singh@sopa4u.com>
2020-01-26 18:18:32 +0530

    dict play done for the correct answer

--- src/commands.js
+++ src/commands.js
@@ -1,5 +1,6 @@
 const chalk = require('chalk');
 const apis = require('./apis');
+const inquirer = require('inquirer');
 
 function processInputCommand(_argv) { 
   if(_argv.length === 4) {
@@ -22,36 +23,26 @@ function processInputCommand(_argv) {
       case 'ex':
         executeExCommand(_word);
       break;
+
+      default:
+        console.log(chalk.red(`\nOOPs! Command not found\n`));
+      break;
     }
 
   } else if(_argv.length === 3){
     let _word = _argv[2];
-    if(_word !== 'play') {
+    if(_word === 'play') {
+      //play game
+      playGame();
+    } else {
       executeDefnCommand(_word);
       executeSynCommands(_word);
       excecuteAntCommand(_word);
       executeExCommand(_word);
-    } else {
-      //play game
-
     }
   } else if(_argv.length === 2) {
-    apis.getRandomWord().then(data => {
-      data = JSON.parse(data);
-
-      let _word = data['word'];
-
-      console.log(chalk.blue(`Random word is "${_word}" \n`))
-      executeDefnCommand(_word);
-      executeSynCommands(_word);
-      excecuteAntCommand(_word);
-      executeExCommand(_word);
-
-    }).catch(err => {
-
-    })
+    executeRandomWordCommand();
   }
-
 }
 
 function relativeWordsProcess(data,_word,_type) {
@@ -129,6 +120,111 @@ function executeExCommand(_word) {
   }).catch(err => {
     console.log(chalk.red(`${err}`));
   })  
+}
+
+function executeRandomWordCommand() {
+  apis.getRandomWord().then(data => {
+    data = JSON.parse(data);
+
+    let _word = data['word'];
+
+    console.log(chalk.blue(`Random word is "${_word}" \n`))
+
+    executeDefnCommand(_word);
+    executeSynCommands(_word);
+    excecuteAntCommand(_word);
+    executeExCommand(_word);
+    
+  }).catch(err => {
+    console.log(chalk.red(`${err}`))
+  })  
+}
+
+async function playGame() {
+  let _display = ['defn','syn','ant'];
+
+  let _random = Math.floor(Math.random()*_display.length);
+
+  try {
+    let _word_data = await apis.getRandomWord();
+    _word_data = JSON.parse(_word_data);
+
+    let _answer = _word_data['word'];
+
+    let _word_definations = await apis.getWordDefinitions(_answer);
+    _word_definations = JSON.parse(_word_definations);
+
+    let _word_syn_ant = await apis.getRelativeWords(_answer);
+    _word_syn_ant = JSON.parse(_word_syn_ant);
+
+    let _word_syn = _word_syn_ant[_word_syn_ant.length-1].words;
+    let _word_ant = (_word_syn_ant.length === 2) ?  _word_syn_ant[0].words: [];
+
+    while(true) {
+      let show = "";
+      if(_display[_random] === 'defn') {
+        show = _word_definations[Math.floor(Math.random()*_word_definations.length)].text;
+      } else if(_display[_random] === 'syn') {
+        show = _word_syn[Math.floor(Math.random()*_word_syn.length)];
+      } else {
+        if(_word_ant.length) {
+          show = _word_ant[Math.floor(Math.random()*_word_ant.length)];
+        } else {
+          show = _word_syn[Math.floor(Math.random()*_word_syn.length)];
+        }
+      }
+  
+      const user_inp = await getUserInput(show,_display[_random]);
+      let answer_is = false;
+      for(let i=0;i<_word_syn.length;i++) {
+        if(_word_syn[i] === user_inp.user_answer) {
+          console.log(chalk.green(`Congratulations! you got that right`));
+          answer_is = true;
+        }
+      }      
+
+      if(!answer_is) {
+        //wrong answer
+        console.log(chalk.red(`Sorry! That is a wrong answer!`))
+
+        
+      }
+
+    }
+    
+  } catch(err) {
+    console.log(chalk.red(`${err}`));
+  }
+}
+
+function getUserInput(show,_type,_correct_answers) {
+  let type = {
+    'defn': 'defination',
+    'syn': 'synonym',
+    'ant': 'antonym'
+  };
+
+  const query = [
+    {
+      name: 'user_answer',
+      type: 'input',
+      message: `Guess the word for the given ${type[_type]}\n\n  ${show}`,
+      validate: (value) => {
+        return true;
+      }
+    }
+  ];
+
+  return inquirer.prompt(query);
+}
+
+function shuffelWord (word){
+  var shuffledWord = '';
+  word = word.split('');
+  while (word.length > 0) {
+    shuffledWord +=  word.splice(word.length * Math.random() << 0, 1);
+  }
+  return shuffledWord;
 }
 
 module.exports = processInputCommand;
